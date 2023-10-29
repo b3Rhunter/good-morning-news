d806d8519983eb884005fd0946ddd33ec7a273b9
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-10-28 20:43:44 -0500

    Add files via upload

--- src/components/StockTicker.js
+++ src/components/StockTicker.js
@@ -2,14 +2,17 @@
 import React from 'react';
 
 const StockTicker = ({ stocks }) => {
+    const allStocks = [...stocks, ...stocks];
     return (
         <div className="stock-ticker">
-            {stocks.map((stock, index) => (
-                <div key={index} className="stock-item">
-                    <span className="stock-symbol">{stock.symbol}</span>
-                    <span className="stock-price">{stock.price}</span>
-                </div>
-            ))}
+            <div className="stock-ticker-content">
+                {allStocks.map((stock, index) => (
+                    <div key={index} className="stock-item">
+                        <span className="stock-symbol">{stock.symbol}</span>
+                        <span className="stock-price">{stock.price}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
